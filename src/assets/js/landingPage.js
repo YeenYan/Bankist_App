675b92f72bf1c477bfeab8f39f4be1a560618b0e
YeenYan <markiantalan@gmail.com>
2022-11-11 20:05:31 +0800

    add style to nav bar & hero section

---
+++ src/assets/js/landingPage.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const heroImage = document.querySelector('.img');
+const mobileImage = './src/assets/mobileSizeimage.webp';
+const largeImage = './src/assets/device.webp';
+
+const maxMobileScreenSize = 600;
+
+(function () {
+  window.onresize = displayWindowSize;
+  window.onload = displayWindowSize;
+
+
+  function displayWindowSize() {
+    let myWidth = window.innerWidth;
+
+    if (myWidth > maxMobileScreenSize) {
+      heroImage.src = largeImage
+    } else {
+      heroImage.src = mobileImage
+    }
+  };
+
+
+})();
